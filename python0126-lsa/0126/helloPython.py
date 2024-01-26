c09dfe8b5e324918f3fe866535340ee618d4a42c
devgiants75 <devgiants75@naver.com>
2024-01-26 14:37:02 +0900

    20240126 hello python!

---
+++ python0126-lsa/0126/helloPython.py
@@ -0,0 +1,17 @@
+print("Hello Python!")
+
+# 주석
+# : 코드에 대한 설명 또는 프로젝트 진행에 대한 정리 등을
+# : 프로그램 컴파일러에게 전달하지 않고 코드 내에서 작성하는 방법
+
+# 주석 작성 or 해제 단축키: ctrl + /
+#
+# 파이썬 실행 단축키: ctrl + shift + f10 / shift + f10
+# (MacOs): command + shift + r
+
+# 설정 단축키: ctrl + alt + s
+
+# 키보드 커서가 어디에 있던 상관없이 문장 한 줄 동작
+# 삭제: ctrl + y
+# 복사: ctrl + c
+# 한 줄 복사: ctrl + d
