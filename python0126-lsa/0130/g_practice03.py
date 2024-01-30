b1b335e99a86c6bba0f2fd9655c6d2ed0342e33c
devgiants75 <devgiants75@naver.com>
2024-01-30 11:43:58 +0900

    20240130

---
+++ python0126-lsa/0130/g_practice03.py
@@ -0,0 +1,23 @@
+# 리스트 & 튜플 예제
+
+# 문제 설명(로직)
+# 식료품점에 구매할 물품과 각각의 가격
+groceries = [("사과", 5000), ("우유", 3000), ("빵", 1500)]
+
+# 각 항목의 가격을 계산
+apple_price = groceries[0][1]
+milk_price = groceries[1][1]
+bread_price = groceries[2][1]
+
+# 총 구매 금액을 계산
+total_price = apple_price + milk_price + bread_price
+
+# 결과 출력
+print(total_price) # 9500
+
+# 연산자 복습 문제
+# BMI(체질량) 계산기 프로그램
+# : 체중(kg)을 키(m)의 제곱으로 나눈 값
+
+# 사용자로 부터 체중과 키를 입력받아 BMI를 계산하는 공식
+
