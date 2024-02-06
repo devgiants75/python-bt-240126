76efd0f727fabc49a74deedac7173856ae961c04
devgiants75 <devgiants75@naver.com>
2024-02-06 10:06:24 +0900

    20240206

--- python0126-lsa/0206/b_practice.py
+++ python0126-lsa/0206/b_practice.py
@@ -6,7 +6,27 @@
 
 # 함수 정의
 # 반환값 X
-# 함수 이름: vending_meachine
+# 함수 이름: vending_machine
 # 매개변수: money(정수)
 
 # ex) 3000원을 넣었을 때: 4잔의 음료수, 잔돈 200원
+
+def vending_machine(money):
+    # 음료수 가격
+    drink_price = 700
+
+    # 음료수 개수 & 잔돈 계산
+    # 계산 결과의 몫 - //
+    # 계산 결과의 나머지 - %
+
+    # 돈을 음료수 가격으로 나눈 몫이 음료수 개수
+    num_drink = money // drink_price
+    # 돈을 음료수 가격으로 나눈 나머지가 잔돈
+    change = money % drink_price
+
+    # 출력
+    print(f'{money}원을 넣었을 때: {num_drink}잔의 음료수, 잔돈 {change}원')
+
+vending_machine(3000)
+vending_machine(5000)
+vending_machine(5600)
