4b57efd10f170a2e8c511db48ce249c450ed276b
devgiants75 <devgiants75@naver.com>
2024-02-07 09:47:33 +0900

    20240207

--- python0126-lsa/0207/a_review.py
+++ python0126-lsa/0207/a_review.py
@@ -2,7 +2,13 @@
 
 # 1. calculate_square 함수 (정사각형의 넓이)
 # : 한 변의 길이를 인자로 받아 정사각형의 넓이를 반환
+def calculate_square(side):
+    return side * side
 
 # 2. calculate_circle 함수 (원의 넓이)
 # : 반지름을 인자로 받아 원의 넓이를 반환
 # : 표준 라이브러리 math에 있는 pi 상수 사용
+import math
+
+def calculate_circle(radius):
+    return radius * radius * math.pi
