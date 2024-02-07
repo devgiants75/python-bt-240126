55d5c1830fc8adda7f28b86589e27a45147a1055
devgiants75 <devgiants75@naver.com>
2024-02-07 09:42:43 +0900

    20240207

---
+++ python0126-lsa/0207/b_result.py
@@ -0,0 +1,13 @@
+# a_review.py 모듈을 사용 #
+
+# 1. import
+# : 변의 길이가 10인 정사각형의 넓이를 계산하고 출력
+# : 반지름이 5인 원의 넓이를 계산하고 출력
+
+# 2. from import
+# : a_review.py 모듈에서 calculate_square 함수만 import하여
+# : 변의 길이가 7인 정사각형의 넓이를 계산하고 출력
+
+# 3. import as
+# : a_review.py 모듈을 a라는 별칭으로 import하여
+# : 반지름이 10인 원의 넓이를 계산하고 출력
