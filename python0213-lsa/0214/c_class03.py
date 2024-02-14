c009815a0987bf3a05338f5f08ba32fca8686fe3
devgiants75 <devgiants75@naver.com>
2024-02-14 10:44:45 +0900

    20240214

--- python0213-lsa/0214/c_class03.py
+++ python0213-lsa/0214/c_class03.py
@@ -27,3 +27,50 @@ print(f'수식 결과는 {result}입니다.')
 # self
 # : 클래스 내부에서 호출한 객체를 찾기 위해 해당 함수를 호출한
 #   객체 그 자체를 가리킴
+
+# 클래스 생성 예제 #
+# 자동차 클래스: Car
+# - 각 자동차의 브랜드, 모델, 연료 타입, 색상 (인스턴스 변수)
+# - 자동차가 수행할 수 있는 기본 동작
+#   : 운전하기, 가속하기, 정지하기 (인스턴스 메서드)
+
+class Car:
+    # 인스턴스 변수 - 명시적으로 정의
+    brand = ''
+    model = ''
+    fuel_type = ''
+    color = ''
+
+    # 인스턴스 메서드
+    def set_datails(self, brand, model, fuel_type, color):
+        # self.brand가 39번 째 줄의 brand 변수를 가리킴
+        self.brand = brand
+        self.model = model
+        self.fuel_type = fuel_type
+        self.color = color
+
+    def drive(self):
+        print(f'{self.brand} {self.model} is driving')
+
+    def accelerate(self):
+        print(f'{self.brand} {self.model} is accelrating')
+
+    def stop(self):
+        print('운행이 종료되었습니다.')
+
+# 자동차 인스턴스 생성 / 세부 정보 설정
+car1 = Car()
+car1.set_datails('Hyundai', 'Sonata', 'Gasoline', 'Blue')
+
+car2 = Car()
+car2.set_datails('Tesla', 'Model S', 'Electric', 'White')
+
+# car1 동작 시연
+car1.drive()
+car1.accelerate()
+car1.stop()
+
+# car2 동작 시연
+car2.drive()
+car2.accelerate()
+car2.stop()
