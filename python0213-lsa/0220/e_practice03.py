53a4a69d64357b623e459edf377437a6b29d34e7
devgiants75 <devgiants75@naver.com>
2024-02-20 11:47:05 +0900

    20240220

--- python0213-lsa/0220/e_practice03.py
+++ python0213-lsa/0220/e_practice03.py
@@ -3,6 +3,17 @@
 
 # reqeusts 라이브러리
 # Beautiful 라이브러리 - bs4 모듈 사용
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.outback.co.kr/'
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+id_element = soup.find(id='dHead')
+print(id_element) # id가 dHead인 태그 내의 모든 HTML 문서
+print(id_element.text) # 해당 HTML 코드 중 내용만 추출
 
 ### 네이버 뉴스 헤드라인 크롤링 ###
 # 네이버 뉴스 > 원하는 기사를 선택
@@ -14,3 +25,18 @@
 # 클래스 선택자
 # - class 속성: .클래스명
 # - id 속성: #아이디명
+
+naver_news='https://n.news.naver.com/mnews/article/008/0005000902'
+response_news = requests.get(naver_news)
+html_news = response_news.text
+soup = BeautifulSoup(html_news, 'html.parser')
+
+headlines = soup.select('.media_end_head_title')
+print(headlines)
+# : select() 메서드는 해당 변수(headline)에 할당된 결과에서
+#   각 요소를 리스트로 반환
+#   >> 해당 리스트를 순회하면서 텍스트의 내용만 추출
+for headline in headlines:
+    print(headline.text.strip())
+    # 문자열.strip()
+    # : 문자열 양끝의 공백과 줄바꿈 제거
---
+++ python0213-lsa/0220/f_practice04.py
@@ -0,0 +1,25 @@
+### 인터넷 영화 데이터베이스에서 영화 정보 수집 ###
+# IMDB: Internet Movie Database
+# https://www.imdb.com/
+
+import requests
+from bs4 import BeautifulSoup
+
+Movie_URL='https://www.imdb.com/'
+headers= {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+}
+
+# 영화 데이터 수집 함수
+def fetch_movie_data(start):
+    url=Movie_URL.format(start)
+    response = requests.get(url, headers=headers)
+    # HTML 내용에 대한 구문을 분석 (내용만 추출)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    movie_containers = soup.select('.ipc-poster-card ipc-poster-card--baseAlt ipc-poster-card--dynamic-width topten-title ipc-sub-grid-item ipc-sub-grid-item--span-2')
+    for movie in movie_containers:
+        image = movie.div.a
+        title = movie.a.span
+        print(f'{image} {title}')
